508154588fc7b32a66ad5d0e395fb0a6df42c519
rathinnn <56340004+rathinnn@users.noreply.github.com>
2020-11-19 22:07:54 +0530

    Finished Forward Kinematics
    Require update for variable increment during plotting

--- MATLAB/STANFORD/Stanford.m
+++ MATLAB/STANFORD/Stanford.m
@@ -4,21 +4,56 @@ clear all
 initparam=[0,90,0.2,0,180,0];
 
 
-figure
-plot3(0,0,0)
 
 NextCenter=[-0.8654;0.3793;0.7620];
 
-for i = 1:1:90
-  NextCenter=[NextCenter multiplot([i,90,0.2+0.005*i,i,180+i,0],NextCenter)];
-  pause(0.01)
-  
+Stanfordmove([90,80,0.2,0,180,0]);
+
+
+
+function Stanfordmove(finalparam)
+
+figure
+plot3(0,0,0)
+NextCenter=[-0.8654;0.3793;0.7620];
+initparam=[0,90,0.2,0,180,0];
+i=1;
+
+while(i==1)
+    i=0;
+    NextCenter=[NextCenter multiplot(initparam,NextCenter)];
+    pause(0.01)
+    for j=1:6
+        if(j==3)
+            if(initparam(3)>finalparam(3))
+                initparam(3)=initparam(3)-0.01;
+                i=1;
+                
+            elseif(initparam(3)<finalparam(3))
+                initparam(3)=initparam(3)+0.01;
+                i=1;
+            end
+            
+        else
+            if(initparam(j)>finalparam(j))
+                initparam(j)=initparam(j)-1;
+                i=1;
+                
+            elseif(initparam(j)<finalparam(j))
+                initparam(j)=initparam(j)+1;
+                i=1;
+            end
+            
+        end
+            
+            
+        
+        
+    end
 end
 
 
-
-
-
+end
 
 
 function [T00,T01,T12,T23,T34,T45,T56,Etip] =  forwardKinematics(theta1,theta2,d3,theta4,theta5,theta6)
@@ -41,48 +76,48 @@ end
 
 function [T] = getTransformMatrix(theta, d, a, alpha)
 T = [cosd(theta) -sind(theta) * cosd(alpha) sind(theta) * sind(alpha) a
- sind(theta) cosd(theta) * cosd(alpha) -cosd(theta) * sind(alpha) a
- 0,sind(alpha),cosd(alpha),d;
- 0,0,0,1];
+    sind(theta) cosd(theta) * cosd(alpha) -cosd(theta) * sind(alpha) a
+    0,sind(alpha),cosd(alpha),d;
+    0,0,0,1];
 end
 
 function nextCenter=oneplot(prevcenter,T)
-    nextCenter=T(1:end-1,end);
-    
-    Centers=[prevcenter nextCenter];
-    
-    x=Centers(1,:);
-    y=Centers(2,:);
-    z=Centers(3,:);
-    hold on
-    
-    plot3(x,y,z,'k','LineWidth',2);
-    
-    xa=nextCenter+T(1:end-1,1)./10;
-    xcenter=[nextCenter xa];
-    xa=xcenter(1,:);
-    ya=xcenter(2,:);
-    za=xcenter(3,:);
-    plot3(xa,ya,za,'r','LineWidth',2)
-    
-    ya=nextCenter+T(1:end-1,2)./10;
-    ycenter=[nextCenter ya];
-    xa=ycenter(1,:);
-    ya=ycenter(2,:);
-    za=ycenter(3,:);
-    plot3(xa,ya,za,'g','LineWidth',2)
-    
-    za=nextCenter+T(1:end-1,3)./10;
-    zcenter=[nextCenter za];
-    xa=zcenter(1,:);
-    ya=zcenter(2,:);
-    za=zcenter(3,:);
-    plot3(xa,ya,za,'b','LineWidth',3)
-    
-    
-    
-    hold off
-    
+nextCenter=T(1:end-1,end);
+
+Centers=[prevcenter nextCenter];
+
+x=Centers(1,:);
+y=Centers(2,:);
+z=Centers(3,:);
+hold on
+
+plot3(x,y,z,'k','LineWidth',2);
+
+xa=nextCenter+T(1:end-1,1)./10;
+xcenter=[nextCenter xa];
+xa=xcenter(1,:);
+ya=xcenter(2,:);
+za=xcenter(3,:);
+plot3(xa,ya,za,'r','LineWidth',2)
+
+ya=nextCenter+T(1:end-1,2)./10;
+ycenter=[nextCenter ya];
+xa=ycenter(1,:);
+ya=ycenter(2,:);
+za=ycenter(3,:);
+plot3(xa,ya,za,'g','LineWidth',2)
+
+za=nextCenter+T(1:end-1,3)./10;
+zcenter=[nextCenter za];
+xa=zcenter(1,:);
+ya=zcenter(2,:);
+za=zcenter(3,:);
+plot3(xa,ya,za,'b','LineWidth',3)
+
+
+
+hold off
+
 
 
 end
