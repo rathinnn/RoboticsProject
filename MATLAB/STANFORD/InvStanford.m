a166f18605d16470134a427608d5ab0b80b6d5b1
rathinnn <56340004+rathinnn@users.noreply.github.com>
2020-11-22 14:28:02 +0530

    Tried Inverse For Stanford

--- MATLAB/STANFORD/InvStanford.m
+++ MATLAB/STANFORD/InvStanford.m
@@ -1,25 +1,153 @@
-close all
-%clear all
+dhparams = [0   	-pi/2	0.762 	0;
+            0	-pi/2       0.394312       0;
+            0	0	0	-pi/2;
+            0   	-pi/2	0.2268	0;
+            0       -pi/2	0   	0;
+            0       0       0.4318     0];
+        
+robot = rigidBodyTree;
+body1 = rigidBody('body1');
+jnt1 = rigidBodyJoint('jnt1','revolute');
 
-%initparam=[0,90,0.2,0,180,0];
+setFixedTransform(jnt1,dhparams(1,:),'dh');
+body1.Joint = jnt1;
+
+addBody(robot,body1,'base')
+
+
+body2 = rigidBody('body2');
+jnt2 = rigidBodyJoint('jnt2','revolute');
+body3 = rigidBody('body3');
+jnt3 = rigidBodyJoint('jnt3','prismatic');
+body4 = rigidBody('body4');
+jnt4 = rigidBodyJoint('jnt4','revolute');
+body5 = rigidBody('body5');
+jnt5 = rigidBodyJoint('jnt5','revolute');
+body6 = rigidBody('body6');
+jnt6 = rigidBodyJoint('jnt6','revolute');
+
+setFixedTransform(jnt2,dhparams(2,:),'dh');
+setFixedTransform(jnt3,dhparams(3,:),'dh');
+setFixedTransform(jnt4,dhparams(4,:),'dh');
+setFixedTransform(jnt5,dhparams(5,:),'dh');
+setFixedTransform(jnt6,dhparams(6,:),'dh');
+
+body2.Joint = jnt2;
+body3.Joint = jnt3;
+body4.Joint = jnt4;
+body5.Joint = jnt5;
+body6.Joint = jnt6;
+
+addBody(robot,body2,'body1')
+addBody(robot,body3,'body2')
+addBody(robot,body4,'body3')
+addBody(robot,body5,'body4')
+addBody(robot,body6,'body5')
+
+showdetails(robot)
+%show(robot);
+randConfig = robot.randomConfiguration;
+%tform = getTransform(robot,randConfig,'L6','base');
+
+show(robot,randConfig);
+
+ik = inverseKinematics('RigidBodyTree',robot);
+weights = [1 1 1 1 1 1];
+initialguess = robot.homeConfiguration;
+initialguess(1).JointPosition = 0;
+initialguess(2).JointPosition = pi/2;
+initialguess(3).JointPosition = 0.2;
+initialguess(4).JointPosition = 0;
+initialguess(5).JointPosition = pi;
+initialguess(6).JointPosition = 0;
+tform = getTransform(robot,randConfig,'body6','base');
+
+
+x1=-0.85;
+y1=0.4;
+z1=0.76;
+
+Etv=[ 
+         0         0   -1.0000   x1;
+   -1.0000         0         0    y1;
+         0    1.0000         0    z1;
+         0         0         0    1.0000
+];
+
+[configSoln,solnInfo] = ik('body6',Etv,weights,initialguess);
+vals = [configSoln(:).JointPosition];
+store=rad2deg(vals);
+
+param=round(store);
+param(3)=round(vals(3),2);
+%NextCenter=[-0.8654;0.3793;0.7620];
+NextCenter=[0;0.3943;0.7967];
 initparam=[0,0,0,0,0,0];
+NextCenter=Stanfordmove(initparam,param,NextCenter);
+param2=param;
+
+x1=-0.85;
+y1=0.2;
+z1=0.76;
+
+Etv=[ 
+         0         0   -1.0000   x1;
+   -1.0000         0         0    y1;
+         0    1.0000         0    z1;
+         0         0         0    1.0000
+];
+weights = [0.1 0.1 0.1 6 0.01 6];
+[configSoln,solnInfo] = ik('body6',Etv,weights,initialguess);
+vals = [configSoln(:).JointPosition];
+store=rad2deg(vals);
+
+param=round(store);
+param(3)=round(vals(3),2);
+
+NextCenter=Stanfordmove(param2,param,NextCenter);
+ 
 
 
 
-NextCenter=[-0.8654;0.3793;0.7620];
 
-Stanfordmove([96  -89   0.01   0 -96 179]);
 
-Stanfordmove(initparam);
-%[T00,T01,T12,T23,T34,T45,T56,Etip] =  forwardKinematics(0,90,0.2,0,180,0);
 
-function Stanfordmove(finalparam)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function NextCenter = Stanfordmove(initparam,finalparam,NextCenter)
 
 
 plot3(0,0,0)
 grid on
-NextCenter=[-0.8654;0.3793;0.7620];
-initparam=[0,90,0.2,0,180,0];
+
+%initparam=[0,90,0.2,0,180,0];
+
 i=1;
 
 while(i==1)
