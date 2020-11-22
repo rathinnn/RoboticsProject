fc60d348b5e40af92f1320cb59b21dea0ea6dca7
rathinnn <56340004+rathinnn@users.noreply.github.com>
2020-11-22 07:43:21 +0530

    Fixed Stanford Updated SCARA
    Forward Kinematics Done
    Inverse Kinematics Needed

--- MATLAB/SCARA/Scara.m
+++ MATLAB/SCARA/Scara.m
@@ -1,37 +1,46 @@
 close all
 clear all
 
-initparam=[0,90,0.2,0,180,0];
+initparam=[0,0,0,0];
 
 
 
-NextCenter=[-0.8654;0.3793;0.7620];
+NextCenter=[0.55;0;0.5];
 
-Stanfordmove([90,80,0.2,0,180,0]);
+Scaramove([45,45,0.4,0]);
+
+[T00,T01,T12,T23,T34,Etip] =  forwardKinematics(0,0,0,0);
 
 
-
-function Stanfordmove(finalparam)
+function Scaramove(finalparam)
 
 figure
 plot3(0,0,0)
-NextCenter=[-0.8654;0.3793;0.7620];
-initparam=[0,90,0.2,0,180,0];
+NextCenter=[0.55;0;0.5];
+initparam=[0,0,0,0];
 i=1;
 
 while(i==1)
     i=0;
     NextCenter=[NextCenter multiplot(initparam,NextCenter)];
     pause(0.01)
-    for j=1:6
+    for j=1:4
         if(j==3)
             if(initparam(3)>finalparam(3))
+                
+                if(abs(initparam(3)-finalparam(3))>0.0000000001)
+                    
                 initparam(3)=initparam(3)-0.01;
                 i=1;
+                end
                 
             elseif(initparam(3)<finalparam(3))
+               
+                if(abs(initparam(3)-finalparam(3))>0.0000000001)
+                    
                 initparam(3)=initparam(3)+0.01;
                 i=1;
+                end
             end
             
         else
@@ -56,21 +65,21 @@ end
 end
 
 
-function [T00,T01,T12,T23,T34,T45,T56,Etip] =  forwardKinematics(theta1,theta2,d3,theta4,theta5,theta6)
+function [T00,T01,T12,T23,T34,Etip] =  forwardKinematics(theta1,theta2,d3,theta4)
 
-d1=0.762;
-d2=0.394312;
-d4=0.2268;
-d6=0.4318;
+d1=0.6;
+d2=0;
+d4=0.1;
+
+
 T00 = [1 0 0 0; 0 1 0 0; 0 0 1 0; 0 0 0 1];
-T01 = getTransformMatrix(theta1,d1,0,-90);
-T12 = getTransformMatrix(theta2,d2,0,-90);
-T23 = getTransformMatrix(-90,d3,0,0);
-T34 = getTransformMatrix(theta4,d4,0,-90);
-T45 = getTransformMatrix(theta5,0,0,-90);
-T56 = getTransformMatrix(theta6,d6,0,0);
+T01 = getTransformMatrix(theta1,d1,0.4,180);
+T12 = getTransformMatrix(theta2,d2,0.15,0);
+T23 = getTransformMatrix(0,d3,0,0);
+T34 = getTransformMatrix(theta4,d4,0,0);
 
-Etip = T00 * T01 * T12 * T23 * T34 * T45 * T56;
+
+Etip = T00 * T01 * T12 * T23 * T34;
 
 end
 
@@ -124,7 +133,7 @@ end
 
 
 function nextCenter= multiplot(param,points)
-[T00,T01,T12,T23,T34,T45,T56,Etip] =  forwardKinematics(param(1),param(2),param(3),param(4),param(5),param(6));
+[T00,T01,T12,T23,T34,Etip] =  forwardKinematics(param(1),param(2),param(3),param(4));
 plot3(0,0,0)
 hold on
 plot3(points(1,:),points(2,:),points(3,:))
@@ -137,10 +146,7 @@ nextCenter=oneplot(nextCenter,T01*T12*T23);
 
 nextCenter=oneplot(nextCenter,T01*T12*T23*T34);
 
-nextCenter=oneplot(nextCenter,T01*T12*T23*T34*T45);
 
-
-nextCenter=oneplot(nextCenter,T01*T12*T23*T34*T45*T56);
 
 axis 'equal'
 x0=0;
@@ -154,7 +160,7 @@ set(gcf,'position',[x0,y0,width,height])
 end
 
 function [T] = initplot(initparam)
-[T00,T01,T12,T23,T34,T45,T56,Etip] =  forwardKinematics(initparam(1),initparam(2),initparam(3),initparam(4),initparam(5),initparam(6));
+[T00,T01,T12,T23,T34,T45,T56,Etip] =  forwardKinematics(initparam(1),initparam(2),initparam(3),initparam(4));
 plot3(0,0,0)
 
 nextCenter=oneplot([0;0;0],T00);
@@ -165,10 +171,7 @@ nextCenter=oneplot(nextCenter,T01*T12*T23);
 
 nextCenter=oneplot(nextCenter,T01*T12*T23*T34);
 
-nextCenter=oneplot(nextCenter,T01*T12*T23*T34*T45);
 
-
-nextCenter=oneplot(nextCenter,T01*T12*T23*T34*T45*T56);
 
 axis 'equal'
 x0=0;
